d800c933b6e32a1c82501182ac14f86bc87f66e3
Sri Tejaswi <abutchiraju15@gmail.com>
2026-08-22 10:32:40 +0530

    LC-maximum-average-subarray-i [Runtime: 2 ms; Memory: 69.6 MB]

---
+++ array/maximum-average-subarray-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+      int sum = 0;
+
+        // Find sum of first k elements
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int maxSum = sum;
+
+        // Sliding window
+        for (int i = k; i < nums.length; i++) {
+
+            sum = sum + nums[i] - nums[i - k];
+
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return (double) maxSum / k;   
+    }
+}
