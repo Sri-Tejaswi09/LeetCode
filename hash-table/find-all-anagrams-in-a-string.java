ea8fa04fdac778d3fc188bec6e9dc8f5223b4f31
Sri Tejaswi <abutchiraju15@gmail.com>
2026-08-22 11:02:36 +0530

    LC-find-all-anagrams-in-a-string [Runtime: N/A; Memory: N/A]

--- hash-table/find-all-anagrams-in-a-string.java
+++ hash-table/find-all-anagrams-in-a-string.java
@@ -3,18 +3,16 @@ import java.util.*;
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
-        if (s.length() < p.length()) return result;
-
+        if (s.length() < p.length()) 
+            return result;
         int[] pCount = new int[26];
         int[] sCount = new int[26];
-
         // Count frequency of characters in p
-        for (char c : p.toCharArray()) {
+        for (char c : p.toCharArray()) 
+        {
             pCount[c - 'a']++;
         }
-
         int window = p.length();
-
         // Initialize first window
         for (int i = 0; i < window; i++) {
             sCount[s.charAt(i) - 'a']++;
@@ -40,10 +38,5 @@ class Solution {
         return result;
     }
 
-    // For local testing
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.findAnagrams("cbaebabacd", "abc")); // [0,6]
-        System.out.println(sol.findAnagrams("abab", "ab"));        // [0,1,2]
-    }
+    
 }
